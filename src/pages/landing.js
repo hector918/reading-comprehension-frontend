2e3f9041cf503ff7b39300053659b7551d4cce98
hector918 <hector_zhong@outlook.com>
2023-07-01 19:24:31 -0400

    landing page renew

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,31 +2,31 @@ import React from "react";
 import './landing.css';
 import MovingGallery from "../components/moving-gallery";
 
-export default function Landing(){
+export default function Landing() {
+  //////////////////////////////////////////
+  const startButtonOnClick = (evt) => {
+    console.log(evt)
+  }
+  //////////////////////////////////////////
   return <div className="landing-container">
-    <div className="moving-gallery-panel"><MovingGallery/></div>
-    <div className="columns">
-      <div className="column">
-        <div className="panel">
-          <div className="panel-header">
-            <div className="panel-title"></div>
-          </div>
-          <div className="panel-nav">
-            <h4>Uses AI read the document</h4>
-            <h6>and anwser questions</h6>
-          </div>
-          <div className="panel-body">
-            -- contents --
-          </div>
-          <div className="panel-footer">
-            <button className="btn-style-h-lg">Start</button>
-          </div>
+    <div className="moving-gallery-panel"><MovingGallery /></div>
+    <div className="floating-panel">
+      <div className="panel">
+        <div className="panel-header">
+          <div className="panel-title"></div>
+        </div>
+        <div className="panel-nav">
+          <p>Uses AI read the document</p>
+          <p>and anwser questions</p>
+        </div>
+        <div className="panel-body">
+          <p>Discover the full story - let AI guide you through every page.</p>
+        </div>
+        <div className="panel-footer">
+          <button className="btn-style-h-lg" onClick={startButtonOnClick}>Start</button>
         </div>
       </div>
-      <div className="column">
-        Second column
-      </div>
-      
     </div>
+    
   </div>
 }
